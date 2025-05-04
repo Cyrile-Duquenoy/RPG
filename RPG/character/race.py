cb175919874999e1c847a13435f0c42e412813f6
Cyrile-Duquenoy <cyrileduquenoy@hotmail.fr>
2025-05-04 17:47:04 +0200

    Update race.py

--- RPG/character/race.py
+++ RPG/character/race.py
@@ -3,14 +3,7 @@ from enum import Enum
 class Race(Enum):
     HUMAIN = 'HUMAIN'
     NAIN = 'NAIN'
-    ELFE_NUIT = 'ELFE_NUIT'
-    DRAENAI = 'DRAENAI'
-    ORC = 'ORC'
-    MORT_VIVANT = 'MORT_VIVANT'
-    TAUREN = 'TAUREN'
-    TROLL = 'TROLL'
-    ELFE_SANG = 'ELFE_SANG'
-    GOBELIN = 'GOBELIN'
+    ELFE = 'ELFE'
     
     def __str__(self):
         return self.value
